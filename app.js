094c6c7b8b7139ca1e73c8e84afd8511c96f48a5
Sunny Koirala <sunnykoirala777@gmail.com>
2025-08-18 20:36:22 +0545

    Inital commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+
+
+//Starting the Server
+app.listen(8080,()=>{
+    console.log("Server is listening to port no 8080");
+});
+
