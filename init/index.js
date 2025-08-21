b169560727b4de4e484aa5415c4b836e9d5fa278
Sunny Koirala <sunnykoirala777@gmail.com>
2025-08-21 16:45:16 +0545

    made some changes

---
+++ init/index.js
@@ -0,0 +1,21 @@
+const mongoose=require("mongoose");
+const Listing=require("../models/listing");
+const allData=require("./data");
+
+
+main().then(()=>{
+    console.log("Successfully connected to DB");
+}).catch((err)=>{
+    console.log(err);
+});
+async function main()
+{
+    await mongoose.connect("mongodb://127.0.0.1:27017/practise");
+}
+
+const initData=async ()=>{
+    await Listing.deleteMany({});
+    await Listing.insertMany(allData.data);
+}
+initData();
+
